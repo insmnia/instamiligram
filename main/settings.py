f75a0b3ce709fa69d1d94c2973803d6656b96fcf
insmnia <teamspirit3209@gmail.com>
2021-10-22 15:28:14 +0300

    heroku deploy

--- main/settings.py
+++ main/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
+import dj_database_url
 from pathlib import Path
 import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -151,8 +152,9 @@ DATABASES = {
 
     }
 }
+db_from_env = dj_database_url.config(conn_max_age=500)
+DATABASES['default'].update(db_from_env)
 # WHITENOISE_USE_FINDERS = True
-# STATIC_ROOT = None
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
 
